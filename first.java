b5d120ea15dbadfa318fd85655fc1f4024fbec3a
shahidkx07 <shahidkhan88892@gmail.com>
2026-01-16 22:07:56 +0530

    JAVA Introduction

---
+++ first.java
@@ -0,0 +1,14 @@
+public class first{
+//FIRST JAVA PROGRAM
+
+/*
+MULTI 
+LINE 
+COMMENT
+
+*/
+    public static void main(String[] args){
+          System.out.println("Shahid");
+          System.out.println("Pathan");
+    }
+}
---
+++ variables.java
@@ -0,0 +1,41 @@
+public class variables {
+  public static void main(String[] args) {
+    int age=20;
+    System.out.println(age);
+    int year=2026;
+    System.out.println("The year is "+year);
+
+
+    double price=88.88;
+    double gpa=7.77;
+
+    System.out.println("THe price of 2 litre milk is "+price);
+  
+   char grade='A';
+   char price2='9';
+   char currency='$';
+
+
+   System.out.println(price2);
+
+   boolean isHuman=false;
+
+   System.out.println(isHuman);
+
+
+   if(isHuman){
+    System.out.println("You are descendent of Adam");
+   }
+   else{
+    System.out.println("You are not descendent of Adam");
+   };
+
+
+
+   String nums="1093";
+   System.out.println(nums);
+}
+
+
+
+}
