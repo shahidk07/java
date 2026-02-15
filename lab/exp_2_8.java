6ed1cf36d29718ac991e8b158c574d0c84f341b7
shahidkx07 <shahidkhan88892@gmail.com>
2026-02-15 19:31:03 +0530

    do while loop with MIN_VALUE AND MAX_VALUE

--- lab/exp_2_8.java
+++ lab/exp_2_8.java
@@ -1,8 +1,37 @@
+// a program that keeps reading numbers from users until he types 0 and find sum, average ,min and max
 import java.util.Scanner;
 import java.util.ArrayList;
 public class exp_2_8 {
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
+        int num,count=0,min,max,sum=0;double avg=0;
+        max=Integer.MIN_VALUE;
+        min=Integer.MAX_VALUE;
+
         
+        do{
+            System.out.println("Enter a number (Enter 0 to exit)\n");
+                num=scn.nextInt();
+            if(num!=0){
+                sum+=num;
+                count+=1;
+                avg=sum/count;
+
+                if(num>max){
+                    max=num;
+                }
+                if(num<min){
+                    min=num;
+                }
+            }
+        }
+        while (num!=0);
+   
+
+
+        System.out.println("Sum::"+sum);
+        System.out.println("Average::"+avg);
+        System.out.println("Maximum ::"+max);
+        System.out.println("Minimum ::"+min);
     }
 }
